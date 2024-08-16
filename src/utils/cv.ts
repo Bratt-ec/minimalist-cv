94954bd01f36e7ecb08988ba1c9a51a04e9d55c8
b.castro <b.castro@mycodedmind.com>
2024-08-16 18:39:36 -0500

    "Update URL in cv.json and cv_en.json, and add console log in cv.ts"

--- src/utils/cv.ts
+++ src/utils/cv.ts
@@ -2,7 +2,8 @@ import * as cvEN from "@cvEN"
 import * as cvES from "@cvES"
 
 export const dataCV = (path:string) => {
+    console.log("path == '/en'",path == '/en');
     let _dataCV = cvES
-    if(path == '/en') _dataCV = cvEN
+    if(path == '/en' ) _dataCV = cvEN
     return  _dataCV
 }
